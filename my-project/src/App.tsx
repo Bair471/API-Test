ab9b2ec61edeb7f438845dd04f004bfaa8fd7acb
Bair471 <bair.shirnen@gmail.com>
2025-09-13 23:21:19 +0800

    Delete not used elements

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -18,15 +18,6 @@ function App() {
         </a>
       </div>
       <PersonTable/>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
